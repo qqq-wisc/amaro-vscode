6819212ba81447d2bad3bed43438465433265374
Keshav Sharma <keshucheek@gmail.com>
2025-12-10 17:28:22 -0600

    Add Backend capabilities to validate document

--- amaro-lsp/src/main.rs
+++ amaro-lsp/src/main.rs
@@ -1,6 +1,19 @@
+use regex::Regex;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct AmaroFile {
+    blocks: Vec<Block>,
+}
+
+#[derive(Debug)]
+struct Block {
+    kind: String,
+    content: String,
+    range: Range,
+}
 
 #[derive(Debug)]
 struct Backend {
@@ -21,16 +34,29 @@ impl LanguageServer for Backend {
         })
     }
 
-    async fn did_open(&self, _: DidOpenTextDocumentParams) {
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
         self.client
             .log_message(MessageType::INFO, "Amaro file opened!")
             .await;
+        // self.validate_document(
+        //     params.text_document.uri,
+        //     params.text_document.text
+        // ).await;
 
         // TODO: Steps  
         // STEP FOR LATER: 
         // 1. Parse params.text_document.text
         // 2. Run type checker
         // 3. Publish Diagnostics (errors)
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        if let Some(change) = params.content_changes.into_iter().next() {
+            // self.validate_document(
+            //     params.text_document.uri,
+            //     change.text
+            // ).await;
+        }
     }
 
     async fn shutdown(&self) -> Result<()> {
